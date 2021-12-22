d9f9d83b98d86638be7714f8f720209e1ed2fc0f
mervecansever <mervecanseverfr@gmail.com>
2021-12-22 21:20:24 +0100

    Added more work

--- src/Work1/Hello.java
+++ src/Work1/Hello.java
@@ -3,5 +3,7 @@ package Work1;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Work1 started");
+        System.out.println("Created Git Repository");
+
     }
 }
